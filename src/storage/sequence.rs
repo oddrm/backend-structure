e4d2b3d88aae5c688b4138b9b90913c08593f224
Daniel Meiborg <mail@danielmeiborg.com>
2026-01-09 17:59:43 +0100

    add methods to sequence

--- src/storage/sequence.rs
+++ src/storage/sequence.rs
@@ -4,4 +4,24 @@ use rocket::serde::{Deserialize, Serialize};
 #[serde(crate = "rocket::serde")]
 pub struct Sequence {}
 
+impl Sequence {
+    pub fn new(description: String, start: Timestamp, end: Timestamp) -> Self {
+        todo!()
+    }
+
+    pub fn description(&self) -> &String {
+        todo!()
+    }
+
+    pub fn start(&self) -> Timestamp {
+        todo!()
+    }
+
+    pub fn end(&self) -> Timestamp {
+        todo!()
+    }
+}
+
 pub type SequenceID = u64;
+// in milliseconds since UNIX EPOCH
+pub type Timestamp = u64;
